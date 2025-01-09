acdc8b932789b476c21e6c5a9c6dba526e0bf789
Mochi <mochitreets0304@gmail.com>
2025-01-09 22:30:25 +0800

    登入token存入cookie

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 import './App.css'
 
 function App() {
+  const [isAdmin, setIsAdmin]=useState(false);
 
   const [account,setAccount]=useState({
     username:'請輸入信箱',
@@ -20,13 +21,29 @@ function App() {
   function login(e){
     e.preventDefault();
     axios.post(`${import.meta.env.VITE_BASE_URL}/v2/admin/signin`,account)
-    .then((res) => console.log(res.message))
-    .catch((err) => console.log(err))
+    .then((res) => {
+      alert(res.data.message);
+      const {token, expired} = res.data;
+      document.cookie = `loginToken=${token}; expires=${new Date(expired)}`;
+      setIsAdmin(true);
+    })
+    .catch((err) => alert('登入失敗  '+err.message))
   }
 
   return (
     <>
-      <h2 className='mb-5 display-2 fw-bold'>登入</h2>
+    {isAdmin ? <h1>ok</h1> : <><h2 className='mb-5 display-2 fw-bold'>登入</h2><form>
+        <div className="form-group mb-3">
+          <label htmlFor="email" className='pb-2'>Email</label>
+          <input type="email" name='username' className="form-control form-control-lg" id="email" onChange={updateEnter} />
+        </div>
+        <div className="form-group mb-3">
+          <label htmlFor="password" className='pb-2'>密碼</label>
+          <input type="password" name='password' className="form-control form-control-lg" id="password" onChange={updateEnter} />
+        </div>
+        <button onClick={login} type="submit" className="btn btn-primary">登入</button>
+      </form></>}
+      {/* <h2 className='mb-5 display-2 fw-bold'>登入</h2>
       <form>
         <div className="form-group mb-3">
           <label htmlFor="email" className='pb-2'>Email</label>
@@ -37,7 +54,7 @@ function App() {
           <input type="password" name='password' className="form-control form-control-lg" id="password" onChange={updateEnter}/>
         </div>
           <button onClick={login} type="submit" className="btn btn-primary">登入</button>
-      </form>
+      </form> */}
     </>
   )
 }
