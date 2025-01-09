26aa34c78d91907a6fac006cdcc21dae68f83ce3
Mochi <mochitreets0304@gmail.com>
2025-01-09 20:17:58 +0800

    套件引入axios,bootstrap等

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:process.env.NODE_ENV==='production' ? '/hex_react_2024/' : '/',
   plugins: [react()],
 })
