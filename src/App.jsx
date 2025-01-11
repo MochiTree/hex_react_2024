e890a15e4ed24a3b293208349f4660c71bfcb497
Mochi <mochitreets0304@gmail.com>
2025-01-11 22:50:05 +0800

    產品頁面登入驗證

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,10 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import axios from 'axios'
 import './App.css'
 
 function App() {
   const [productDetail, setDetail] = useState(null);
   const [products, setProducts] = useState([])
-  // const [products, setProducts] = useState([
-  //   {
-  //     category: "甜甜圈",
-  //     content: "尺寸：14x14cm",
-  //     description: "濃郁的草莓風味，中心填入滑順不膩口的卡士達內餡，帶來滿滿幸福感！",
-  //     id: "-L9tH8jxVb2Ka_DYPwng",
-  //     is_enabled: 1,
-  //     origin_price: 150,
-  //     price: 99,
-  //     title: "草莓莓果夾心圈",
-  //     unit: "元",
-  //     num: 10,
-  //     imageUrl: "https://images.unsplash.com/photo-1583182332473-b31ba08929c8",
-  //     imagesUrl: [
-  //       "https://images.unsplash.com/photo-1626094309830-abbb0c99da4a",
-  //       "https://images.unsplash.com/photo-1559656914-a30970c1affd"
-  //     ]
-  //   },
-  //   {
-  //     category: "蛋糕",
-  //     content: "尺寸：6寸",
-  //     description: "蜜蜂蜜蛋糕，夾層夾上酸酸甜甜的檸檬餡，清爽可口的滋味讓人口水直流！",
-  //     id: "-McJ-VvcwfN1_Ye_NtVA",
-  //     is_enabled: 1,
-  //     origin_price: 1000,
-  //     price: 900,
-  //     title: "蜂蜜檸檬蛋糕",
-  //     unit: "個",
-  //     num: 1,
-  //     imageUrl: "https://images.unsplash.com/photo-1627834377411-8da5f4f09de8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80",
-  //     imagesUrl: [
-  //       "https://images.unsplash.com/photo-1618888007540-2bdead974bbb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=987&q=80",
-  //     ]
-  //   },
-  //   {
-  //     category: "蛋糕",
-  //     content: "尺寸：6寸",
-  //     description: "法式煎薄餅加上濃郁可可醬，呈現經典的美味及口感。",
-  //     id: "-McJ-VyqaFlLzUMmpPpm",
-  //     is_enabled: 1,
-  //     origin_price: 700,
-  //     price: 600,
-  //     title: "暗黑千層",
-  //     unit: "個",
-  //     num: 15,
-  //     imageUrl: "https://images.unsplash.com/photo-1505253149613-112d21d9f6a9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fGNha2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60",
-  //     imagesUrl: [
-  //       "https://images.unsplash.com/flagged/photo-1557234985-425e10c9d7f1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA5fHxjYWtlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60",
-  //       "https://images.unsplash.com/photo-1540337706094-da10342c93d8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDR8fGNha2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60"
-  //     ]
-  //   }
-  // ]);
 
   const [isAdmin, setIsAdmin]=useState(false);
 
@@ -82,7 +30,7 @@ function App() {
       document.cookie = `loginToken=${token}; expires=${new Date(expired)}`;
       setIsAdmin(true);
 
-      axios.defaults.headers.common['Authorization'] = token;
+      axios.defaults.headers.common['Authorization'] = `${token}`;
 
       axios.get(`${import.meta.env.VITE_BASE_URL}/v2/api/${import.meta.env.VITE_API_PATH}/admin/products`)
       .then((res)=>setProducts(res.data.products))
@@ -91,9 +39,30 @@ function App() {
     .catch((err) => alert('登入失敗  '+err.message))
   }
 
+  //箭頭函式寫法
+  // const loginCheck = async() =>{
+  //   try{
+  //     await axios.post(`${import.meta.env.VITE_BASE_URL}/v2/api/user/check`)
+  //     alert('已登入')
+  //   }catch(err) {
+  //     alert(err.message)
+  //     setIsAdmin(false)
+  //   }
+  // }
+
+  async function loginCheck(){
+    try{
+      await axios.post(`${import.meta.env.VITE_BASE_URL}/v2/api/user/check`);
+      alert('已登入');
+    } catch(err) {
+      alert(err.message)
+      setIsAdmin(false)
+    }
+  }
+
   return (
     <>
-    {isAdmin ? (<><div className="row">
+    {isAdmin ? (<><button className='btn btn-danger mb-3' onClick={loginCheck}>檢查登入狀態</button><div className="row">
             <div className='col col-8'>
               <h1 className='fw-bold'>產品列表</h1>
         <table className="table">
@@ -156,18 +125,6 @@ function App() {
         </div>
         <button onClick={login} type="submit" className="btn btn-primary">登入</button>
       </form></>}
-      {/* <h2 className='mb-5 display-2 fw-bold'>登入</h2>
-      <form>
-        <div className="form-group mb-3">
-          <label htmlFor="email" className='pb-2'>Email</label>
-          <input type="email" name='username' className="form-control form-control-lg" id="email" onChange={updateEnter}/>
-        </div>
-        <div className="form-group mb-3">
-          <label htmlFor="password" className='pb-2'>密碼</label>
-          <input type="password" name='password' className="form-control form-control-lg" id="password" onChange={updateEnter}/>
-        </div>
-          <button onClick={login} type="submit" className="btn btn-primary">登入</button>
-      </form> */}
     </>
   )
 }
