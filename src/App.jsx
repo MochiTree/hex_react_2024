afc4d836cbb30ce2042866c72737f8c171ba1c24
Mochi <mochitreets0304@gmail.com>
2025-01-12 21:03:41 +0800

    點擊編輯顯示modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { useState } from 'react'
-import axios from 'axios'
-import './App.css'
+import { useState,useEffect,useRef } from 'react';
+import axios from 'axios';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Modal} from 'bootstrap';
 
 function App() {
   const [productDetail, setDetail] = useState(null);
@@ -10,11 +12,17 @@ function App() {
     status:false,
   });
   const [isAdmin, setIsAdmin]=useState(false);
+  const modalRef =useRef(null);
+  const modalRefMethod = useRef(null);
 
   const [account,setAccount]=useState({
     username:'請輸入信箱',
     password:'請輸入密碼',
   })
+
+  useEffect(()=>{
+    modalRefMethod.current = new Modal(modalRef.current)
+  },[])
 
   function updateEnter(e){
 
@@ -61,7 +69,7 @@ function App() {
       alert(err.message)
       setIsAdmin(false)
     }
-  }
+  };
   async function backEnd(){
     try{
       await axios.post(`${import.meta.env.VITE_BASE_URL}/v2/api/user/check`);
@@ -75,16 +83,36 @@ function App() {
           display:'block',
           status:!isBackEnd.status,
         });
-      }
+      };
     } catch(err) {
       alert(err.message)
       setIsAdmin(false)
     }
-  }
+  };
+
+  function showModal(){
+    modalRefMethod.current.show();
+  };
 
   return (
     <>
-    { isAdmin ? (<><button className='btn btn-success mb-3 me-2' onClick={backEnd}>{isBackEnd.status ? '產品頁面' : '後台頁面'}</button><button className='btn btn-danger mb-3' onClick={loginCheck}>檢查登入狀態</button><div className="row">
+     <div className="modal fade" ref={modalRef} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal-dialog">
+                <div className="modal-content">
+                <div className="modal-header">
+                    <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div className="modal-body">
+                    ...
+                </div>
+                <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" className="btn btn-primary">Save changes</button>
+                </div>
+                </div>
+            </div>
+            </div> { isAdmin ? (<><button className='btn btn-success mb-3 me-2' onClick={backEnd}>{isBackEnd.status ? '產品頁面' : '後台頁面'}</button><button className='btn btn-danger mb-3' onClick={loginCheck}>檢查登入狀態</button><div className="row">
             <div className='col col-8'>
               <h1 className='fw-bold'>產品列表</h1>
         <table className="table">
@@ -105,7 +133,7 @@ function App() {
       <td>{item.price}</td>
       <td>{item.is_enabled}</td>
       <td><button className='btn btn-primary btn-sm' onClick={()=>setDetail(item)}>查看細節</button></td>
-      <td><button className='btn btn-primary btn-sm' style={{display:`${isBackEnd.display}`}}>編輯</button></td>
+      <td><button className='btn btn-primary btn-sm' style={{display:`${isBackEnd.display}`}} onClick={showModal}>編輯</button></td>
     </tr>)
     })}
   </tbody>
